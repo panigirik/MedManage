9d098080ec4b6905ead2d033a4ffd5229334ea35
panigirik <zaharvaskovskij78@gmail.com>
2026-06-14 18:03:43 +0300

    .gitignore  updates

--- MedManage.Core/MedManage.Application/obj/Debug/net8.0/MedManage.Application.AssemblyInfo.cs
+++ MedManage.Core/MedManage.Application/obj/Debug/net8.0/MedManage.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MedManage.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6a6e7aedea7118d658a46adb88311f44f39228a")]
 [assembly: System.Reflection.AssemblyProductAttribute("MedManage.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MedManage.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- MedManage.Core/MedManage.Domain/obj/Debug/net8.0/MedManage.Domain.AssemblyInfo.cs
+++ MedManage.Core/MedManage.Domain/obj/Debug/net8.0/MedManage.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MedManage.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6a6e7aedea7118d658a46adb88311f44f39228a")]
 [assembly: System.Reflection.AssemblyProductAttribute("MedManage.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MedManage.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
